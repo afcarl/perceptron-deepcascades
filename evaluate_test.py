c58566137d329bab6769bbb0e7fb64799b94253d
alemc2 <alemc2@gmail.com>
2015-12-20 21:46:09 -0500

    adding test results and updating evaluate to show std

--- evaluate_test.py
+++ evaluate_test.py
@@ -8,10 +8,10 @@ import glob
 import collections
 import statistics
 
-pat = re.compile(r'cascade_(.*)_train_([0-9])_([0-9.]+)')
+pat = re.compile(r'cascade_(.*)_train_([0-9.]+)')
 
 if __name__ == '__main__':
-    files = glob.glob('results/*')
+    files = glob.glob('test_results/*')
     results = collections.defaultdict(lambda: collections.defaultdict(list))
     for fname in files:
         dc = None
@@ -28,16 +28,17 @@ if __name__ == '__main__':
         fname = fname.split('/')[-1]
         groups = pat.match(fname).groups()
         origin = groups[0]
-        fold = int(groups[1])
-        gamma = float(groups[2])
+        gamma = float(groups[1])
         #if gamma != 1.00 and gamma != 0.01: continue
         #if origin != 'ionosphere_train': continue
-        test_file = origin + '_test_%d' % fold
-        (X_train, y_train, X_test, y_test) = pdc.load_dataset(test_file, test_file)
+        test_file = origin + '_test'
+        train_file = origin + '_train'
+        (X_train, y_train, X_test, y_test) = pdc.load_dataset(train_file, test_file)
         print
-        print origin, fold,gamma
+        print origin, gamma
         print
         (e, mk_m) = dc.error(X_train, y_train)
+        (e, mk_m) = dc.error(X_test, y_test)
         results[origin][gamma].append(e)
         if gamma != dc.gamma:
             print 'SHOULD NOT HAPPEN'
@@ -46,4 +47,4 @@ if __name__ == '__main__':
     print '\nCross-validation errors:'
     for (origin, gamma_map) in results.items():
         for (gamma, error) in gamma_map.items():
-            print origin, gamma, statistics.mean(error), statistics.stdev(error)
+            print origin, gamma, error
